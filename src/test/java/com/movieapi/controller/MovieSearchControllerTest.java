dc77d66c2cd77916327869a01735d55d69550453
Jaakko Rajala <jaakko.rajala@tuni.fi>
2025-07-09 12:55:49 +0300

    test(controller): refactor MovieControllerTest and MovieSearchControllerTest to use MovieTestDataBuilder
    Closes #7

--- src/test/java/com/movieapi/controller/MovieSearchControllerTest.java
+++ src/test/java/com/movieapi/controller/MovieSearchControllerTest.java
@@ -9,8 +9,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
@@ -33,10 +33,10 @@ class MovieSearchControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private MovieService movieService;
 
-    @MockBean
+    @MockitoBean
     private MovieSearchValidator movieSearchValidator;
 
     @Autowired
